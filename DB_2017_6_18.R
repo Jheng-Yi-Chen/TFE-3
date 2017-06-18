f74033b5d69b7ef878c9c141e21cfda7961ddde9
qwtehvn <deanst28@gmail.com>
2017-06-18 13:05:22 +0800

    Add files via upload

--- DB_2017_6_18.R
+++ DB_2017_6_18.R
@@ -1,10 +1,27 @@
 
-library(foreign)
-db <- read.dta("C:/Users/CJY/Desktop/db/registration_new.dta")
+##################################################
 
-library(xlsx)
-install.packages("rJava", .libPaths()[1], "http://www.rforge.net/")
-library(rJava)
-require(rJava)
-write.xlsx(db, file="C:/Users/CJY/Desktop/db/db.xlsx", sheetName="db", col.names=TRUE, row.names=FALSE)
-write.csv(db, file="C:/Users/CJY/Desktop/db/db.csv")
+library(foreign)
+library(ggplot2)
+library(dplyr)
+library(plotly)
+library(highcharter)
+
+##################################################
+
+DB1 <- read.csv("https://raw.githubusercontent.com/qwtehvn/DB/master/CustomQuery.csv")
+
+head(DB1)
+str(DB1)
+
+##################################################
+
+
+
+
+##################################################
+
+
+
+
+
